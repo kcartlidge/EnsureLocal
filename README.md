dd45b0aca2c405f15e63b06e94cb2e3a5803634e
K Cartlidge <kcartlidge@users.noreply.github.com>
2023-05-25 07:33:30 +0100

    Scans folders, reads 1 byte from each file, multiple tries with backoff

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
 # EnsureLocal
 
-OneDrive is very poor at making sure files are always available locally.
-Even though the option is available it often fails to work fully.
-Other cloud services may not even have the option at all.
+**Ready to use**, despite only being a few commits. (No more were necessary.)
 
-*EnsureLocal* scans the folder structure starting where it is run from.
+OneDrive is poor at making sure files are always available locally.
+Even though the option exists, it often fails to work fully and will also remove local versions of files if it considers space is getting tight on your machine.
+Other cloud services may not have the option at all.
+
+*EnsureLocal* scans a nested folder structure.
 The first byte of every file found is read (and ignored).
-*This ensures every file is locally synced*.
+*This forces a local sync to occur*.
+
+If a file cannot be read it tries again after 2 seconds and then after another 10 seconds, allowing cloud syncing to catch up if it is running slow.
+
+Folders/files with names starting `.` are skipped.
+
+You can stop the run at any time with Ctrl+C.
+Restarting will whizz through previously accessed files as they are now local.
+
+If run without a parameter, it will prompt for the folder.
 
 [View the license (AGPL)](./LICENSE.txt).
 
@@ -22,8 +33,15 @@ cd mac-arm      # Switch for your OS
 
 ## Rebuilding locally
 
-Clone the repo and run the relevant [scripts](./scripts) folder entry.
+Clone the repo and run the relevant [scripts](./scripts) folder entry (scripts exist for running on *Mac*, *Linux*, and *Windows*).
 You *may* need to apply `chmod +x` to the script file for Mac or Linux.
+
+Scripts should be run from the top level folder (where this `README` lives).
+
+``` sh
+cd <repo>
+./scripts/mac.sh
+```
 
 ## Running straight from source
 
--- main.go
+++ main.go
@@ -2,14 +2,145 @@
 
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+var (
+	buff  []byte
+	debug bool = false
+)
 
 func main() {
 	fmt.Println()
-	fmt.Println("ENSURE LOCAL")
-	fmt.Println("Scans the complete file structure starting")
-	fmt.Println("where it is run from. Reads the first byte")
-	fmt.Println("of each file found. This should ensure any")
-	fmt.Println("cloud service syncs files locally.")
+	fmt.Println(" _____                        __                _ ")
+	fmt.Println("|   __|___ ___ _ _ ___ ___   |  |   ___ ___ ___| |")
+	fmt.Println("|   __|   |_ -| | |  _| -_|  |  |__| . |  _| .'| |")
+	fmt.Println("|_____|_|_|___|___|_| |___|  |_____|___|___|__,|_|")
 	fmt.Println()
+	fmt.Println("Scans a nested file structure and reads byte 1 of each file.")
+	fmt.Println("Cloud files will therefore be synced locally.")
+	fmt.Println()
+	fmt.Println("File read failures are retried twice (cloud sync may be catching up).")
+	fmt.Println("The first retry is after 2 seconds, the second after 10 seconds.")
+	fmt.Println()
+	fmt.Println("Folders/files with names starting '.' are skipped.")
+	fmt.Println()
+	fmt.Println("Usage:  EnsureLocal [folder]")
+	fmt.Println("        (If no folder is provided you will be prompted)")
+	fmt.Println()
+
+	folder := ""
+	if len(os.Args) != 2 {
+		cwd, _ := os.Getwd()
+		fmt.Println("The current working folder (.) is:")
+		fmt.Println(cwd)
+		fmt.Println()
+		fmt.Println("Folder (absolute, or relative to the above): ")
+		n, err := fmt.Scanln(&folder)
+		fmt.Println()
+		if n != 1 || err != nil {
+			os.Exit(2)
+		}
+	} else {
+		folder = os.Args[1]
+	}
+	absFolder, err := filepath.Abs(folder)
+	check(err)
+
+	fmt.Println()
+	fmt.Println("SCANNING:")
+	fmt.Println(absFolder)
+	fmt.Println()
+	fmt.Println("Stop the run at any time using Ctrl+C.")
+	fmt.Println()
+	fmt.Println("Cloud/local speed/volumes will impact time taken.")
+	fmt.Println("Press Enter/Return to continue (or Ctrl+C)")
+	fmt.Println()
+	fmt.Scanln()
+
+	buff = make([]byte, 1)
+	err = filepath.WalkDir(folder, func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			if d == nil {
+				// Errors reading the root.
+				return err
+			} else {
+				showIssue(path, err.Error())
+			}
+		}
+
+		// Skip anything whose path inludes a dotted portion.
+		segments := strings.Split(path, string(filepath.Separator))
+		include := true
+		for _, segment := range segments {
+			if segment == "." || strings.HasPrefix(segment, ".") {
+				include = false
+				break
+			}
+		}
+		if include {
+			if d.IsDir() {
+				fmt.Println(path)
+			} else {
+				fi, err := d.Info()
+				if err != nil {
+					showIssue(path, err.Error())
+				} else if fi.Size() > 0 {
+					// Make 3 attempts, with a 2 second and then a 10 second back-off.
+					err = readFirstByte(path)
+					if err != nil {
+						time.Sleep(time.Second * 2)
+						err = readFirstByte(path)
+						if err != nil {
+							time.Sleep(time.Second * 10)
+							err = readFirstByte(path)
+							showIssue(path, err.Error())
+						}
+					}
+				}
+				if debug {
+					fmt.Printf("  %x  %s\n", buff, path)
+				}
+			}
+		}
+		return nil
+	})
+	check(err)
+
+	fmt.Println()
+	fmt.Println("Done")
+	fmt.Println()
+}
+
+func showIssue(path string, errorMessage string) {
+	fmt.Printf("* %s -- %s", path, errorMessage)
+}
+
+func readFirstByte(path string) error {
+	f, err := os.Open(path)
+	if err == nil {
+		n, err := f.Read(buff)
+		if err == nil {
+			if n != 1 {
+				return errors.New("could not read first byte of file: " + path)
+			}
+		}
+	}
+	return err
+}
+
+func check(err error) {
+	if err != nil {
+		fmt.Println()
+		fmt.Println("ERROR")
+		fmt.Println(err.Error())
+		fmt.Println()
+		os.Exit(1)
+	}
 }
